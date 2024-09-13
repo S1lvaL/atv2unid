9b8f6b55118646e4cf48152f9a0c9688bb248d2e
S1lvaL <152618404+S1lvaL@users.noreply.github.com>
2024-09-13 10:20:48 -0300

    Add files via upload

---
+++ 2unid/src/main/java/com/mycompany/atividade01/Atividade01.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.atividade01;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author laila
+ */
+public class Atividade01 {
+    public static void main(String[] args) {
+        int quant = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de nomes a ser cadastrada"));
+        String nomes[] = new String[quant];
+
+        for (int i = 0; i < quant; i++) {
+            nomes[i] = JOptionPane.showInputDialog("Digite o nome da pessoa " + (i + 1) + ": ");
+        }
+
+        while (true) {
+            int posicao = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe a posição do nome a ser consultado ou -1 para sair"));
+
+            if (posicao == -1) {
+                break;
+            } else if (posicao >= 1 && posicao <= quant) {
+                JOptionPane.showMessageDialog(null, "Nome registrado na posição " + posicao + " é " + nomes[posicao - 1]);
+            } else {
+                JOptionPane.showMessageDialog(null, "Posição invalida");
+            }
+        }
+    }
+}
